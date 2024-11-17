462d0f0c3a1a950d21972b55f4b7b70d11539488
Jayme Fortes <Jayme.fortes@edu.pucrs.br>
2024-11-17 20:49:56 -0300

    mcomeçando implementação em drones

--- src/dados/DroneCarga.java
+++ src/dados/DroneCarga.java
@@ -1,11 +1,28 @@
 package dados;
 
 public abstract class DroneCarga extends Drone {
-
 	private double pesoMaximo;
+
+	public DroneCarga(int codigo, double custoFixo, double autonomia,double pesoMaximo) {
+		super(codigo, custoFixo, autonomia);
+		this.pesoMaximo = pesoMaximo;
+	}
+
+	public double getPesoMaximo() {
+		return pesoMaximo;
+	}
+
+	public void setPesoMaximo(double pesoMaximo) {
+		this.pesoMaximo = pesoMaximo;
+	}
 
 	@Override
 	public double calculaCustoKm() {
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Peso Máximo: " + String.format("%.2f", pesoMaximo) + " KG";
 	}
 }
--- src/dados/DronePessoal.java
+++ src/dados/DronePessoal.java
@@ -1,51 +1,27 @@
 package dados;
 import java.util.Locale;
 
-public class DronePessoal extends Drone {
+public class DronePessoal extends DroneCarga {
+	private int qtdPessoas;
 
-	private int QTDPessoas;
-	private double custoFinal;
-	private String descricao;
-	private double peso;
-
-	public DronePessoal(int codigo, double peso, double autonomia, int QTDPessoas, String descricao, double custoFixo, double custoFinal) {
-		super(codigo, custoFixo, autonomia);
-		this.peso = peso;
-		this.QTDPessoas = QTDPessoas;
-		this.descricao = descricao;
-		this.custoFinal = custoFinal;
+	public DronePessoal(int codigo, double custoFixo, double autonomia, double pesoMaximo,int qtdPessoas) {
+		super(codigo, custoFixo, autonomia, pesoMaximo);
+		this.qtdPessoas = qtdPessoas;
 	}
 
-	public int getQTDPessoas() {
-		return QTDPessoas;
+	public int getQtdPessoas() {
+		return qtdPessoas;
 	}
-	public void setQTDPessoas(int QTDPessoas) {
-		this.QTDPessoas = QTDPessoas;
-	}
-	public double getCustoFinal() {
-		return custoFinal;
-	}
-	public void setCustoFinal(double custoFinal) {
-		this.custoFinal = custoFinal;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public double getPeso() {
-		return peso;
-	}
-	public void setPeso(double peso) {
-		this.peso = peso;
-	}
-
-	public String toString() {
-		return "Código: " + getCodigo() + ", Peso suportado: " + String.format(Locale.US, "%.2f",getPeso()) + " Kg, Distancia de autonomia: " + String.format(Locale.US, "%.2f",getAutonomia()) + " Km, " + getQTDPessoas() + " Pessoas, Descrição: " + getDescricao() ;
+	public void setQtdPessoas(int QTDPessoas) {
+		this.qtdPessoas = QTDPessoas;
 	}
 
 	public double calculaCustoKm() {
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Quantidade de pessoas: " + qtdPessoas;
 	}
 }
